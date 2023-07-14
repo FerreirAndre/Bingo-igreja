adf8e9a6b24a43b1467eaa936e06dd8e1bd839cc
FerreirAndre <adrecruz15@gmail.com>
2023-07-14 12:22:09 -0300

    fazendo alterações, adicionando cores,a palavra bingo e classes para edições no css

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const letters = document.querySelector(".letters-bingo");
 let array = Array.apply(null, { length: 76 }).map(Number.call, Number);
 array.shift();
 
-let iterator = 0;
+iterator = 0;
+let letras = ['B', 'I', 'N', 'G', 'O'];
 
 for (let i = 0; i < 5; ++i) {
   let tr = document.createElement("tr");
   table.appendChild(tr);
-
+  let letra = document.createElement("td");
+  letra.id = letras[i].toString();
+  letra.classList.add("main-table-cell");
+  letra.classList.add("letra");
+  letra.textContent = letras[i];
+  tr.appendChild(letra);
   for (let j = 0; j < 15; ++j) {
     let td = document.createElement("td");
     td.id = array[iterator].toString();
     td.classList.add("main-table-cell");
-
     let div = document.createElement("div");
     div.classList.add("cell-format");
+    div.classList.add(letras[i]);
     div.textContent = array[iterator].toString();
     td.appendChild(div);
     tr.appendChild(td);
@@ -24,16 +30,11 @@ for (let i = 0; i < 5; ++i) {
   }
 }
 
-const cell = document.querySelectorAll(".main-table-cell");
+const cell = document.querySelectorAll(".cell-format");
 
 
 cell.forEach(e => {
   e.addEventListener("click", () => {
     e.classList.toggle("strikeout");
-
-    if (e.classList.contains("strikeout"))
-      console.log(`numero ${e.id} foi adicionado`);
-    else
-      console.log(`numero ${e.id} foi removido`)
   })
 })
